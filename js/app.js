16ff5952f2b293235b8c8b9861d8ac4e960fc483
“Erika <erika.olivo@cmpc.cl>
2026-01-28 11:08:11 -0300

    Modificación de la ruta al SW

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 
+var url = window.location.href;
+// Para peoducción
+var swLocation = '/cursopwa/sw.js';
+
 if ( navigator.serviceWorker ){
-    navigator.serviceWorker.register('/sw.js');
+    if (url.includes('localhost')){
+        swLocation = '/sw.js';
+    }
+    navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
